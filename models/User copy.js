ece648b43fbb769940989127e13d8144a2b6e41d
Mark Ciubal <mark.ciubal@gmail.com>
2023-07-28 07:39:12 -0700

    Working on User routes.

---
+++ models/User copy.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const assignmentSchema = require('./Assignment');
+const { ObjectId } = require('mongoose').Types;
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+      max_length: 50
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [emailRegex, 'Please enter a valid email address.']
+    },
+    thoughts: {
+      type: [{ type: ObjectId, ref: 'Thoughts' }]
+    },
+    friends: {
+      type: [{ type: ObjectId, ref: 'Friends' }]
+    }
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
